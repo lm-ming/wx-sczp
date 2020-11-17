82ae15c2845ed44a362462456461c151e5e093d4
lm-ming <lm18748041440@163.com>
2020-11-17 17:45:12 +0800

    首页导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,18 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    zw_list: [
+      {id:"1",name:"军人专区"},
+      {id:"2",name:"UI设计师"},
+      {id:"3",name:"军人专区"}
+    ],
+    isChecked: 0,
+    isDisplay: 0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  navList(e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      isChecked: e.target.dataset.id,
+      isDisplay: e.target.dataset.id
     })
   }
 })
